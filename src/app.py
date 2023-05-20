937152ea94cc79ff38470f8134f987bb119d840b
jackesfonseca <jackesdafonseca@gmail.com>
2023-05-20 20:33:57 -0300

    changing layout and adding remove task option

--- src/app.py
+++ src/app.py
@@ -6,15 +6,28 @@ root.title("To Do List")
 
 def add_task():
     task = entry_task.get()
-
-    listbox_tasks.insert(tkinter.END, task)
+    if task != "":
+        listbox_tasks.insert(tkinter.END, task)
+        entry_task.delete(0, tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(title="Atenção!", message="Insira uma tarefa")
 
 def remove_task():
-    pass
+    task_index = listbox_tasks.curselection()
+    listbox_tasks.delete(task_index)
 
 # GUI
-listbox_tasks = tkinter.Listbox(root, height=3, width=50)
-listbox_tasks.pack()
+frame_tasks = tkinter.Frame()
+frame_tasks.pack()
+
+listbox_tasks = tkinter.Listbox(frame_tasks, height=10, width=50)
+listbox_tasks.pack(side=tkinter.LEFT)
+
+scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
+scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
+scrollbar_tasks.config(command=listbox_tasks)
 
 entry_task = tkinter.Entry(root, width=50)
 entry_task.pack()
